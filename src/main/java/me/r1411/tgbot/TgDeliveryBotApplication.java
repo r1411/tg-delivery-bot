092ae88bd801f133a82b76e097e3f0db98d82018
Roman <59415030+r1411@users.noreply.github.com>
2023-07-05 10:17:24 +0300

    Задание 2. Страница Hello, World! (#1)
    * Задание 2. Страница Hello, World!
    
    * Изменение версии Spring Boot на 2.7.13

---
+++ src/main/java/me/r1411/tgbot/TgDeliveryBotApplication.java
@@ -0,0 +1,16 @@
+package me.r1411.tgbot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Main Spring Boot application class
+ */
+@SpringBootApplication
+public class TgDeliveryBotApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TgDeliveryBotApplication.class, args);
+	}
+
+}
---
+++ src/test/java/me/r1411/tgbot/TgDeliveryBotApplicationTests.java
@@ -0,0 +1,13 @@
+package me.r1411.tgbot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TgDeliveryBotApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
